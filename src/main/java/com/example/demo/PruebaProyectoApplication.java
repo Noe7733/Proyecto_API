65291d43b7c0f27e77e31fbe0c78f7526fad5c45
Noe7733 <noe777guillenwigetta@gmail.com>
2022-11-21 08:55:17 -0600

    documentacion en Swagger

--- src/main/java/com/example/demo/PruebaProyectoApplication.java
+++ src/main/java/com/example/demo/PruebaProyectoApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PruebaProyectoApplication {
 
+	// Este es el link para acceder a la documentacion Swagger
+	// http://localhost:8080/swagger-ui.html
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaProyectoApplication.class, args);
 	}
