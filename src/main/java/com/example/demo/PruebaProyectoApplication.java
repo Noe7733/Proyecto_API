9becacc778ac4abfb17b719c3d82681258c84fc1
Noe7733 <noe777guillenwigetta@gmail.com>
2022-11-28 22:07:25 -0600

    link swagger con heroku

--- src/main/java/com/example/demo/PruebaProyectoApplication.java
+++ src/main/java/com/example/demo/PruebaProyectoApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PruebaProyectoApplication {
 
 	// Este es el link para acceder a la documentacion Swagger
-	// http://localhost:8080/swagger-ui.html
+	// https://unachotify-1.herokuapp.com/swagger-ui.html
+	//el swagger si jala con heroku xD
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaProyectoApplication.class, args);
